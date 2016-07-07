979fe4922cf81791091290e74c34b196065ef6b9
Rodrigo Dugin <rodrigodugin@sect-42.sect.rj.gov.br>
2016-07-07 14:09:39 -0700

    Initial Commit

---
+++ SOSBattery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SOSBattery
+//
+//  Created by Rodrigo Dugin on 07/07/16.
+//  Copyright © 2016 Point-Break Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          // Do any additional setup after loading the view, typically from a nib.
+     }
+
+     override func didReceiveMemoryWarning() {
+          super.didReceiveMemoryWarning()
+          // Dispose of any resources that can be recreated.
+     }
+
+
+}
+
